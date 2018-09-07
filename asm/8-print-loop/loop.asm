b7feef45ab1311bd01aff3120661b5ff4088a200
Oleg Gashev <oleg@gashev.net>
2018-09-07 17:19:38 +0000

    Added asm loop example.

---
+++ asm/8-print-loop/loop.asm
@@ -0,0 +1,38 @@
+extern exit
+extern printf
+
+SECTION .data
+    format: db '%d', 10, 0
+
+SECTION .bss
+    index: resb 4
+
+SECTION .text
+    GLOBAL _start
+
+_start:
+    ; Init index.
+    mov eax, 0
+    mov [index], eax
+
+loop:
+    ; Print number.
+    mov rsi, [index]
+    mov rdi, format
+    mov eax, 0
+    call printf
+
+    ; Increment index.
+    mov eax, [index]
+    inc eax
+    mov [index], eax
+
+    ; Compare index value.
+    cmp eax, 10
+    jl loop
+
+end:
+    ; Exit application.
+    mov edi, 0
+    call exit
+
