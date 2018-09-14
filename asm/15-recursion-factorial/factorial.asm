c5e033e5f8a2fdbef9ef3c4ecf8f2be16ffd3eda
Oleg Gashev <oleg@gashev.net>
2018-09-14 06:13:22 +0000

    Aded asm example to calc factorial use recursion.

---
+++ asm/15-recursion-factorial/factorial.asm
@@ -0,0 +1,51 @@
+extern exit
+extern printf
+
+SECTION .data
+    format: db '%d! = %d', 10, 0
+
+
+SECTION .text
+    GLOBAL _start
+
+_start:
+    ; Init index.
+    mov rdi, 7
+    call factorial
+
+    ; Print factorial
+    mov rdx, rax        ; Store factorial
+    mov rsi, rdi        ; Store index
+    mov rdi, format     ; Output format
+    mov rax, 0
+    call printf
+ 
+end:
+    ; Exit application.
+    mov rdi, 0
+    call exit
+
+factorial:
+    push rdi
+
+    cmp rdi, 1
+    jg prev_factorial
+
+    mov rax, 1
+
+end_factorial:
+    pop rdi
+    ret
+
+prev_factorial:
+    push rbx
+    push rdi
+
+    mov rbx, rdi
+    dec rdi
+    call factorial 
+    mul rbx
+    pop rdi
+    pop rbx
+ 
+    jmp end_factorial
